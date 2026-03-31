b706eb28ed95879b5b1cfb10cbb38c11a9f4c51e
ashu <ashuyasashwini@gmail.com>
2026-03-31 13:25:16 +0530

    fixed signup issues again

--- backend/server.js
+++ backend/server.js
@@ -69,7 +69,7 @@ const startServer = async () => {
     await connectDB();
 
     app.listen(PORT, () => {
-      console.log(🚀 Server running on port ${PORT});
+      console.log('🚀 Server running on port ${PORT}');
     });
 
   } catch (error) {
